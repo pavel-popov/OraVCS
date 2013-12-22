c9858483d776a6485bc12984b3a95a2fec5e4c83
Pavel Popov <pavelpopov@outlook.com>
2013-12-22 16:45:43 +0400

    Added git exporting and command-line support

--- oravcs/oravcs.py
+++ oravcs/oravcs.py
@@ -5,31 +5,74 @@
     OraVCS
 """
 
-import sys
 import os
 import re
 import subprocess
 import logging
 import yaml
-
 import shutil
 import argparse
 
+import sh
 import cx_Oracle
 
 __author__ = 'Pavel Popov'
 __email__ = 'pavelpopov@outlook.com'
 __license__ = 'GPL'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 __status__ = 'Prototype'
 
 
+# HOME DIR
+ORAVCS_HOME = os.path.dirname(__file__)
+
+# config dictionary
+CONFIG = None
+
 # logging
-LOGFORMAT = '%(asctime)s %(levelname)-10s %(module)-10s %(funcName)-20s: %(message)s'
+LOGFORMAT = '%(asctime)s %(levelname)-10s %(funcName)-20s: %(message)s'
 LOGLEVEL = logging.DEBUG
 
 logger = logging.getLogger(__name__)
 logger.setLevel(LOGLEVEL)
+
+
+# some various general functions
+
+def key(dict, key, default=None):
+    if key in dict:
+        return dict[key]
+    else:
+        return default
+
+
+def rmdir(path):
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+
+
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def debug(fn):
+    def inner(*args, **kwargs):
+        logger.debug('Running %s', str(fn))
+
+        if args:
+            logger.debug('Args: %s', str(args))
+        if kwargs:
+            logger.debug('Kwargs: %s', str(kwargs))
+
+        try:
+            r = fn(*args, **kwargs)
+        except Exception, e:
+            logger.error('Error occured: %s - %s', str(e.args), e.message)
+
+        logger.debug('Finished %s with result %s', str(fn), str(r))
+
+    return inner
 
 
 def logger_setup(logger, format=LOGFORMAT, datefmt='%Y-%m-%d %H:%M:%S'):
@@ -39,24 +82,12 @@ def logger_setup(logger, format=LOGFORMAT, datefmt='%Y-%m-%d %H:%M:%S'):
     stream_handler.setLevel(LOGLEVEL)
     stream_handler.setFormatter(formatter)
 
-    file_handler = logging.FileHandler(__file__[:-3]+'.log')
+    file_handler = logging.FileHandler(key(CONFIG, 'log', __file__[:-3]+'.log'))
     file_handler.setLevel(LOGLEVEL)
     file_handler.setFormatter(formatter)
 
     logger.addHandler(stream_handler)
     logger.addHandler(file_handler)
-
-
-def debug(fn):
-    def inner(*args, **kwargs):
-        logger.debug('Running %s', str(fn))
-        logger.debug('Args: %s', str(args))
-        logger.debug('Kwargs: %s', str(kwargs))
-        r = fn(*args, **kwargs)
-        logger.debug('Finished %s with result %s', str(fn), str(r))
-
-    return inner
-
 
 @debug
 def sqlplus_exec(connection_string, script, *args):
@@ -71,76 +102,53 @@ def sqlplus_exec(connection_string, script, *args):
 
 
 @debug
-def install(config, **kwargs):
+def install(args):
     """Install Oracle schema objects"""
 
-    if 'basedir' in kwargs:
-        basedir = kwargs['basedir']
-    else:
-        basedir = os.path.join(os.path.dirname(__file__), '..', 'install')
-
+    basedir = os.path.join(ORAVCS_HOME, '..', 'install')
     os.chdir(basedir)
 
-    if 'create_user' in kwargs:
-        connection_string = config['install']['dba']
-        datafile = config['install']['datafile']
-        username, password = re.split('\W+', config['connection'])[0:2]
+    if args.create_user:
+        connection_string = CONFIG['install']['dba']
+        datafile = CONFIG['install']['datafile']
+        username, password = re.split('\W+', CONFIG['connection'])[0:2]
         sqlplus_exec(connection_string, 'create_user.sql', username, password, datafile)
 
     # installing schema
-    connection_string = config['connection']
+    connection_string = CONFIG['connection']
     sqlplus_exec(connection_string, 'build_all.sql')
 
 
-def load_config(filename='oravcs.yaml'):
-    return yaml.load(open(filename).read())
-
-
-def rmdir(path):
-    if os.path.isdir(path):
-        shutil.rmtree(path)
-
-
-def mkdir(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
+not_hidden = lambda x, args: x['name'][-1] != '~' and (args.SCHEMA is None or x['name'] in args.SCHEMA)
 
 
 @debug
-def export_schema(config, schema):
-    basedir = config['export']
-    connection_string = config['connection']
-    schema_name = schema['name']
-    
-    logger.info('Processing schema %s', schema_name)
-    
-    schema_dir = os.path.join(basedir, schema_name)
+def export_schema(schema):
+    logger.info('Processing schema %s', schema['name'])
+
+    connection_string = CONFIG['connection']
+    schema_dir = os.path.join(ORAVCS_HOME, CONFIG['export'], schema['name'])
+
     rmdir(schema_dir)
     mkdir(schema_dir)
     
     build_all = open(os.path.join(schema_dir, 'build_all.sql'), 'w')
     build_all_lines = ['WHENEVER OSERROR EXIT 1\n', 'WHENEVER SQLERROR EXIT 2\n']
 
-    if 'ddl_filter' in schema:
-        ddl_filter = schema['ddl_filter']
-    else:
-        ddl_filter = config['ddl_filter']
-
-    if 'ddl_order' in schema:
-        ddl_order = schema['ddl_order']
-    else:
-        ddl_order = config['ddl_order']
+    ddl_filter = key(schema, 'ddl_filter', CONFIG['ddl_filter'])
+    ddl_order = key(schema, 'ddl_order', CONFIG['ddl_order'])
 
     connection = cx_Oracle.connect(connection_string)
     connection.autocommit = 0
     cursor = connection.cursor()
 
-    if 'download_only' not in schema or not schema['download_only']:
+    if key(schema, 'regenerate', True):
+        logger.info('Regenerating object DDLs')
         cursor.execute('DELETE FROM oravcs_metadata WHERE UPPER(schema)=UPPER(:schema)', schema=schema['name'])
         cursor.execute('BEGIN oravcs_process(UPPER(:schema)); END;', schema=schema['name'])
         cursor.execute('COMMIT')
 
-    logger.debug('Schema: %s', schema)
+    # logger.debug('Schema: %s', schema)
     query = '''
             SELECT obj_type,
                    obj_name,
@@ -168,7 +176,7 @@ def export_schema(config, schema):
 
         build_all_lines.append('@%s.sql\n' % os.path.join(row[0], row[1]))
 
-        logger.debug('Processed file: %s', filename)
+        logger.info('Processed file: %s', filename)
 
     build_all_lines.append('EXIT 0\n')
     build_all.writelines(build_all_lines)
@@ -178,20 +186,130 @@ def export_schema(config, schema):
 
 
 @debug
-def export(config):
-    es = lambda x: export_schema(config=config, schema=x)
-    not_hidden = lambda x: x['name'][-1] != '~'
-    map(es, filter(not_hidden, config['schema']))
+def export(args):
+    map(export_schema, filter(lambda x: not_hidden(x, args), CONFIG['schema']))
 
 
 @debug
-def main():
-    config = load_config()
-    # install(config['oravcs'], create_user=True)  # tested - works
-    # install(config['oravcs'])
-    export(config['oravcs'])
+def commit_schema(schema):
+    logger.info('Processing schema %s', schema['name'])
 
+    mkdir(os.path.join(ORAVCS_HOME, CONFIG['repos']))
+
+    if 'git' not in schema:
+        logger.info('No git setup on schema %s', schema['name'])
+        return
+
+    schema_dir = os.path.join(ORAVCS_HOME, CONFIG['repos'], schema['name'])
+    export_dir = os.path.join(ORAVCS_HOME, CONFIG['export'], key(schema['git'], 'from_schema', schema['name']), '')
+    git = sh.git.bake(_cwd=schema_dir)
+    branch = schema['git']['branch']
+    repo = schema['git']['repo']
+
+    if repo[0:5] == 'https':
+        pass
+    else:  # ssh access
+        # todo: add support of ssh passphrase
+        pass
+
+    if not os.path.exists(schema_dir):  # new repo dir
+        try:
+            mkdir(schema_dir)
+            git.init()
+            git.remote.add.origin(repo)
+            git.fetch()
+            git.checkout(branch)
+        except sh.ErrorReturnCode, e:
+            rmdir(schema_dir)
+            logger.fatal('Error %s', e.args[0])
+            return
+    else:  # existing repo dir
+        try:
+            git.reset('--hard')
+            git.checkout(branch)
+            git.fetch()
+            git.pull.origin(branch)
+        except sh.ErrorReturnCode, e:
+            logger.fatal('Error %s', e.args[0])
+            return
+
+    try:
+        if not key(schema['git'], 'subdir'):
+            dir = os.path.join(schema_dir, '')
+        else:
+            dir = os.path.join(schema_dir, schema['git']['subdir'])
+
+        sh.rm('-rf', dir)
+        mkdir(dir)
+        sh.cp('-r', export_dir, dir)
+    except:
+        raise
+
+    status = git.status('--porcelain')
+
+    if status == '':  # nothing to commit
+        logger.info('Nothing to commit - exiting')
+        return
+
+    # staging modified files
+
+    def add(item):
+        if item[0] == 'D':
+            git.rm(item[1])
+        else:
+            git.add(item[1])
+
+    logger.info('Git status\n%s', git.status())
+    lines = [tuple(x.strip().split(' ')) for x in status.split('\n') if x != '']
+    map(add, lines)
+    logger.info('Git status after staging\n%s', git.status())
+
+    # commit
+    git.commit('-m', key(schema['git'], 'comment', 'OraVCS autocommit'))
+    git.info('Commit succeeded')
+
+    if key(schema['git'], 'push', True):
+        git.push.origin(branch)
+        git.info('Branch pushed to origin')
+
+
+@debug
+def commit(args):
+    map(commit_schema, filter(lambda x: not_hidden(x, args), CONFIG['schema']))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--config', default='oravcs.yaml', help='YAML config file (default: %(default)s)')
+
+    subparsers = parser.add_subparsers(help='Operation mode')
+
+    def add_schemas_arg(parser):
+        parser.add_argument('--schemas', nargs='*', dest='SCHEMA',
+                            help='Schemas to be exported. Default - all schemas without ~ at end')
+
+    parser_install = subparsers.add_parser('install', help='Install OraVCS schema into db')
+    parser_install.add_argument('--create_user', action='store_true', help='Create user during install process')
+    parser_install.set_defaults(func=install)
+
+    parser_export = subparsers.add_parser('export', help='Export schemas from db')
+    add_schemas_arg(parser_export)
+    parser_export.set_defaults(func=export)
+
+    parser_commit = subparsers.add_parser('commit', help='Commit export result to git')
+    add_schemas_arg(parser_commit)
+    parser_commit.set_defaults(func=commit)
+
+    args = parser.parse_args()
+
+    global CONFIG
+    CONFIG = yaml.load(open(args.config).read())
+
+    logger_setup(logger)
+
+    result = args.func(args)
+    return result
 
 if __name__ == '__main__':
-    logger_setup(logger)
     main()
